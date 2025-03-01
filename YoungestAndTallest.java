68cdb8c97990e711c8ea5bf3f3ef2a5403b9edba
mulasumit12 <mulasumit2019@gmail.com>
2025-03-01 11:31:21 +0530

    [Sumit]  feat: added youngest and tallest friend program

---
+++ YoungestAndTallest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class YoungestAndTallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age1 = sc.nextInt(), height1 = sc.nextInt();
+        int age2 = sc.nextInt(), height2 = sc.nextInt();
+        int age3 = sc.nextInt(), height3 = sc.nextInt();
+        int youngest = Math.min(age1, Math.min(age2, age3));
+        int tallest = Math.max(height1, Math.max(height2, height3));
+        System.out.println(youngest);
+        System.out.println(tallest);
+    }
+}
